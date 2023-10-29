dd516604db451c35bf487bc44f8675ef1a934e7c
kinick <kimjun9809@gmail.com>
2023-10-29 14:45:52 +0900

    proflie 이미지를 둥글게 변형하고 클릭시 profile 이미지를 새 화면에 크게 띄우는 예제

--- app/src/main/java/kr/ac/wku/libraryparctice_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/libraryparctice_20231029/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.ac.wku.libraryparctice_20231029
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -12,5 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.imgPro.setOnClickListener {
+
+//            이미지뷰도 클릭 이벤트 지원
+
+//            ViewPhotoActivity로 이동
+
+            val myIntent = Intent( this, ViewPhotoActivity::class.java )
+            startActivity(myIntent)
+
+        }
     }
 }
