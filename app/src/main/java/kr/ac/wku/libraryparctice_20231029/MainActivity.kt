f754c777922beb9eab07ca480e289acd3bae6b14
kinick <kimjun9809@gmail.com>
2023-10-29 14:19:54 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/wku/libraryparctice_20231029/MainActivity.kt
@@ -0,0 +1,16 @@
+package kr.ac.wku.libraryparctice_20231029
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.libraryparctice_20231029.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
